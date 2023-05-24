3a0977a99c3be45db055835cf8e06e807684a135
AlexSchieder <alex.schieder@posteo.net>
2023-05-24 19:54:53 +0200

    add faq page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import FAQ from '../views/FAQ.vue'
 
 const history = createWebHistory();
 const routes = [
 	{ path: "/", component: Home },
 	{ path: "/login", component: Login },
+	{ path: "/faq", component: FAQ },
 ];
 const router = createRouter({ history, routes });
 
