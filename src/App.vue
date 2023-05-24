fe996b2bb6bcb7f6fb7b38b2b4337ae42191325e
AlexSchieder <alex.schieder@posteo.net>
2023-05-24 17:22:37 +0200

    Update structure

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,26 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import ThemeBtn from './components/ThemeSwitchButton.vue'
-import Navigation from './components/BaseNavigation.vue'
+import MainNavigation from './components/MainNavigation.vue'
+import BaseDropdown from './components/BaseDropdown.vue';
 </script>
 
 <template>
-  <Navigation />
-  <ThemeBtn />
-  <router-view></router-view>
+  <a href="#main" class="sr-only">skip to content</a>
+  <header class="bg-gray-200 flex justify-end">
+    <MainNavigation />
+    <ThemeBtn />
+    <!-- <BaseDropdown /> -->
+  </header>
+
+  <main id="main" class="mx-3">
+   <router-view></router-view>
+  </main>
 </template>
+
+<style lang="scss">
+.content-wrapper {
+  margin: 0 auto;
+  max-width: 95vw;
+}
+</style>
---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Button from './BaseButton.vue'
+import Navigation from './BaseNavigation.vue';
+
+const isOpen = ref(false)
+
+const links = ref([{ name: 'Home', link: '/' }, { name: 'Login', link: '/login' }, { name: 'Other', link: '/' }])
+
+</script>
+
+<template>
+	<Button
+		class="button-rounded relative z-10 bg-gray-200 hover:bg-gray-300 lg:hidden"
+		@click="isOpen = !isOpen"
+	>
+		<span class="sr-only">Open main menu</span>
+		<svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+		</svg>
+		<svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+		</svg>
+	</Button>
+
+	<div class="lg:hidden absolute w-full">
+		<nav 
+			aria-label="Main Navigation"
+			:class="isOpen ? 'block': 'sr-only'"
+			class="bg-gray-100 p-2"
+		>
+			<ul class="ml-2 bg-gray-200 dark:bg-gray-700 rounded">
+				<li v-for="item in links"
+					class="p-2 first:rounded-t last:rounded-b hover:(bg-gray-300 dark:bg-gray-600)"
+				>
+					<router-link :to="item.link" class="w-full inline-block">{{ item.name }}</router-link>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="<lg:hidden w-full lg:flex items-center">
+		<nav 
+			aria-label="Main Navigation"
+			class=""
+		>
+			<ul class="flex justify-start bg-gray-200 dark:bg-gray-700">
+				<li v-for="item in links"
+					class="p-2 m-2 hover:(bg-gray-300 dark:bg-gray-600)"
+				>
+					<router-link :to="item.link" class="">{{ item.name }}</router-link>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</template>
